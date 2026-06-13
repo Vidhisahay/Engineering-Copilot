2c1f9ee164593105cc9077949e8fdc4cfc8fecae
Vidhi Sahay <vidhisahay25@gmail.com>
2026-06-13 12:56:02 +0530

    Added the requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Engineering-Copilot",
+    version="0.1.0",
+    author="Vidhi Sahay",
+    author_email="vidhisahay25@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
